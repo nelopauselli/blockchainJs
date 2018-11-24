126dbbebff7447b1e48eb93c9760cb82a148e8c1
Nelo Pauselli <nelopauselli@gmail.com>
2018-11-24 00:38:12 -0300

    agregando comentarios

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Node = require("./blockchainNode");
 const Transaction = require("./transaction");
 
+// creando 2 nodos de la red
 var node1 = new Node("nelo");
 var node2 = new Node("pedro", node1);
 
+// agregando transacciones a la red por distintos nodos
 node1.add(new Transaction("nelo", "juan", 23));
 node2.add(new Transaction("nelo", "pedro", 17));
 
@@ -13,11 +15,16 @@ node2.mine(); // no debería minar nada porque ya minó el nodo-1
 node1.add(new Transaction("pedro", "juan", 10));
 node2.mine(); // mina la transacción que acaba de entrar por el nodo-1
 
+// agregamos una transacción que no será minada
+node1.add(new Transaction("juan", "nelo", 1));
+
+// recorremos las cuentas y mostramos el balance en uno y otro nodo. Debería ser el mismo
 for(let account of ["nelo", "pedro", "juan"]){
     console.log(`the balance of '${account}' in 'node1' is ${node1.getBalanceOfAddress(account)}`);
     console.log(`the balance of '${account}' in 'node2' is ${node2.getBalanceOfAddress(account)}`);
 }
 
+// mostramos toda la blockchain
 for (let block of node1.blockchain.chain) {
     for (let document of block.documents){
         console.log(document);
